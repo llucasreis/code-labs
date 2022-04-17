3174a9d3e2adfda3cb3dc2ff7bea609d9d245100
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-04-17 16:55:55 -0300

    chore: add initial project config

---
+++ python/yolo_detection_with_mqtt_pub/src/config/log.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s] %(message)s')
+
+logger = logging.getLogger(__name__)
---
+++ python/yolo_detection_with_mqtt_pub/tests/test_yolo_detection_with_mqtt_pub.py
@@ -0,0 +1,5 @@
+from yolo_detection_with_mqtt_pub import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
